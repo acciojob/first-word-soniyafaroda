1d433f75370dab52fc1e12b41463b5b030a67065
Soniya Faroda <143260442+soniyafaroda@users.noreply.github.com>
2025-12-15 15:38:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    if (str.length === 0) return "";
+
+    // Remove leading spaces
+    str = str.trimStart();
+
+    // Find first space
+    let index = str.indexOf(" ");
+
+    // If no space found, return whole string
+    if (index === -1) return str;
+
+    // Return first word
+    return str.substring(0, index);
 }
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
